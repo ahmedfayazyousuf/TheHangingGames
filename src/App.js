59fbb129954e50dc91ef16a8c7ce83d5fa8ce97c
ahmedfayazyousuf <95180025+ahmedfayazyousuf@users.noreply.github.com>
2024-01-03 11:14:24 +0400

    o
    o

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import Home from './components/Home'; 
 import Crossword from './components/Crossword'; 
 import Wordsearch from './components/Wordsearch'; 
+import WhackAMole from './components/WhackAMole'; 
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
           <Route exact path="/" element={<Home />} />
           <Route exact path="/Wordsearch" element={<Wordsearch />} />
           <Route exact path="/Crossword" element={<Crossword />} />
+          <Route exact path="/WhackAMole" element={<WhackAMole />} />
         </Routes>
       </Router>
     </>
---
+++ src/components/WhackAMole/index.jsx
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import './styles.css'; // Import your CSS file
+
+const WhackAMoleGame = () => {
+  const [holes, setHoles] = useState([]);
+  const [score, setScore] = useState(0);
+  const [timeUp, setTimeUp] = useState(false);
+  const [lastHole, setLastHole] = useState(null);
+
+  useEffect(() => {
+    const holes = document.querySelectorAll('.hole');
+    setHoles(holes);
+  }, []);
+
+  const randomTime = (min, max) => Math.round(Math.random() * (max - min) + min);
+
+  const randomHole = (holes) => {
+    const idx = Math.floor(Math.random() * holes.length);
+    const hole = holes[idx];
+    if (hole === lastHole) {
+      console.log('Ah nah, that\'s the same one bud');
+      return randomHole(holes);
+    }
+    setLastHole(hole);
+    return hole;
+  };
+
+  const peep = () => {
+    if (!timeUp) {
+      const time = randomTime(200, 1000);
+      const hole = randomHole(holes);
+      hole.classList.add('up');
+      setTimeout(() => {
+        hole.classList.remove('up');
+        if (!timeUp) peep();
+      }, time);
+    }
+  };
+
+  const startGame = () => {
+    setScore(0);
+    setTimeUp(false);
+    setLastHole(null);
+    peep();
+    setTimeout(() => setTimeUp(true), 10000);
+  };
+
+  const bonk = (e) => {
+    if (!e.isTrusted) return; // cheater!
+    setScore(score + 1);
+    e.target.parentNode.classList.remove('up');
+  };
+
+  return (
+    <div>
+      <h1>Whack-a-mole! <span className="score">{score}</span></h1>
+      <div className="controls">
+        <button onClick={startGame} style={{width: '100px', height: '30px', backgroundColor: 'black', color: '#00f526', border: '1px solid #00f526', cursor: 'grab'}}>Start!</button>
+      </div>
+      <div className="game">
+        {Array.from({ length: 6 }, (_, index) => (
+          <div key={index} className={`hole hole${index + 1}`}>
+            <div className="mole" onClick={bonk}></div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default WhackAMoleGame;
