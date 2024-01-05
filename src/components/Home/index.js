4906f202383ff1e5e4783dcef02c70e10a8741f1
ahmedfayazyousuf <95180025+ahmedfayazyousuf@users.noreply.github.com>
2024-01-05 13:00:40 +0400

    o
    o

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -28,103 +28,103 @@ const Home = () => {
 
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'row', flexWrap: 'wrap', minWidth: '80vw', maxWidth: '100vw', paddingBottom: '100px'}}>
           <div style={{height: '100px', width: '160px', display: 'flex', flexDirection: 'column', margin: '5px', padding: '10px', justifyContent: 'center', alignItems: 'center', backgroundImage: `url(${Crossword})`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', borderRadius: '15px', border: '1px solid grey'}}>
-            <Link to="/Crossword" target='_blank' className='start-btn' style={{textDecoration: 'none'}}>
+            <Link to="/Crossword" target='_blank' className='start-btn' style={{textDecoration: 'none', zIndex: '3'}}>
               CROSSWORD
             </Link>
-            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', gap: '2px', marginTop: '5px', opacity: '0.7'}}>
-              <img style={{width: '17px', position: 'sticky'}} src={MobileIcon}/>
-              <img style={{width: '20px', position: 'sticky'}} src={DesktopIcon}/>
+            <div style={{height: '100px', width: '160px', display: 'flex', flexDirection: 'row', margin: '5px', padding: '10px', zIndex: '1', position: 'absolute', justifyContent: 'flex-end', alignItems: 'flex-end', borderRadius: '15px'}}>
+              <img style={{width: '16px', marginRight: '2px', position: 'sticky'}} alt='someicon' src={MobileIcon}/>
+              <img style={{width: '18px', position: 'sticky'}} alt='someicon' src={DesktopIcon}/>
             </div>
           </div>
             
           <div style={{height: '100px', width: '160px', display: 'flex', flexDirection: 'column', margin: '5px', padding: '10px', justifyContent: 'center', alignItems: 'center', backgroundImage: `url(${Wordsearch})`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', borderRadius: '15px', border: '1px solid grey'}}>
-            <Link to="/WordSearch" target='_blank' className='start-btn' style={{textDecoration: 'none'}}>
+            <Link to="/WordSearch" target='_blank' className='start-btn' style={{textDecoration: 'none', zIndex: '3'}}>
               WORDSEARCH
             </Link>
-            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', gap: '2px', marginTop: '5px', opacity: '0.7'}}>
-              <img style={{width: '17px', position: 'sticky'}} src={MobileIcon}/>
-              <img style={{width: '20px', position: 'sticky'}} src={DesktopIcon}/>
+            <div style={{height: '100px', width: '160px', display: 'flex', flexDirection: 'row', margin: '5px', padding: '10px', zIndex: '1', position: 'absolute', justifyContent: 'flex-end', alignItems: 'flex-end', borderRadius: '15px'}}>
+              <img style={{width: '16px', marginRight: '2px', position: 'sticky'}} alt='someicon' src={MobileIcon}/>
+              <img style={{width: '18px', position: 'sticky'}} alt='someicon' src={DesktopIcon}/>
             </div>
           </div>
 
 
           <div style={{height: '100px', width: '160px', display: 'flex', flexDirection: 'column', margin: '5px', padding: '10px', justifyContent: 'center', alignItems: 'center', backgroundImage: `url(${Whackamole})`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', borderRadius: '15px', border: '1px solid grey'}}>
-            <Link to="/whackamole" target='_blank' className='start-btn' style={{textDecoration: 'none'}}>
+            <Link to="/whackamole" target='_blank' className='start-btn' style={{textDecoration: 'none', zIndex: '3'}}>
               WHACK-A-MOLE
             </Link>
-            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', gap: '2px', marginTop: '5px', opacity: '0.7'}}>
-              <img style={{width: '17px', position: 'sticky'}} src={MobileIcon}/>
-              <img style={{width: '20px', position: 'sticky'}} src={DesktopIcon}/>
+            <div style={{height: '100px', width: '160px', display: 'flex', flexDirection: 'row', margin: '5px', padding: '10px', zIndex: '1', position: 'absolute', justifyContent: 'flex-end', alignItems: 'flex-end', borderRadius: '15px'}}>
+              <img style={{width: '16px', marginRight: '2px', position: 'sticky'}} alt='someicon' src={MobileIcon}/>
+              <img style={{width: '18px', position: 'sticky'}} alt='someicon' src={DesktopIcon}/>
             </div>
           </div>
 
           <div style={{height: '100px', width: '160px', display: 'flex', flexDirection: 'column', margin: '5px', padding: '10px', justifyContent: 'center', alignItems: 'center', backgroundImage: `url(${Hangman})`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', borderRadius: '15px', border: '1px solid grey'}}>
-            <Link to="/hangman" target='_blank' className='start-btn' style={{textDecoration: 'none'}}>
+            <Link to="/hangman" target='_blank' className='start-btn' style={{textDecoration: 'none', zIndex: '3'}}>
               HANGMAN
             </Link>
-            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', gap: '2px', marginTop: '5px', opacity: '0.7'}}>
-              <img style={{width: '17px', position: 'sticky'}} src={MobileIcon}/>
-              <img style={{width: '20px', position: 'sticky'}} src={DesktopIcon}/>
+            <div style={{height: '100px', width: '160px', display: 'flex', flexDirection: 'row', margin: '5px', padding: '10px', zIndex: '1', position: 'absolute', justifyContent: 'flex-end', alignItems: 'flex-end', borderRadius: '15px'}}>
+              <img style={{width: '16px', marginRight: '2px', position: 'sticky'}} alt='someicon' src={MobileIcon}/>
+              <img style={{width: '18px', position: 'sticky'}} alt='someicon' src={DesktopIcon}/>
             </div>
           </div>
 
           <div style={{height: '100px', width: '160px', display: 'flex', flexDirection: 'column', margin: '5px', padding: '10px', justifyContent: 'center', alignItems: 'center', backgroundImage: `url(${MemoryGame})`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', borderRadius: '15px', border: '1px solid grey'}}>
-            <Link to="/memorygame" target='_blank' className='start-btn' style={{textDecoration: 'none'}}>
+            <Link to="/memorygame" target='_blank' className='start-btn' style={{textDecoration: 'none', zIndex: '3'}}>
               MEMORY GAME
             </Link>
-            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', gap: '2px', marginTop: '5px', opacity: '0.7'}}>
-              <img style={{width: '17px', position: 'sticky'}} src={MobileIcon}/>
-              <img style={{width: '20px', position: 'sticky'}} src={DesktopIcon}/>
+            <div style={{height: '100px', width: '160px', display: 'flex', flexDirection: 'row', margin: '5px', padding: '10px', zIndex: '1', position: 'absolute', justifyContent: 'flex-end', alignItems: 'flex-end', borderRadius: '15px'}}>
+              <img style={{width: '16px', marginRight: '2px', position: 'sticky'}} alt='someicon' src={MobileIcon}/>
+              <img style={{width: '18px', position: 'sticky'}} alt='someicon' src={DesktopIcon}/>
             </div>
           </div>
 
           <div style={{height: '100px', width: '160px', display: 'flex', flexDirection: 'column', margin: '5px', padding: '10px', justifyContent: 'center', alignItems: 'center', backgroundImage: `url(${TicTacToe})`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', borderRadius: '15px', border: '1px solid grey'}}>
-            <Link to="/tictactoe" target='_blank' className='start-btn' style={{textDecoration: 'none'}}>
+            <Link to="/tictactoe" target='_blank' className='start-btn' style={{textDecoration: 'none', zIndex: '3'}}>
               TIC TAC TOE
             </Link>
-            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', gap: '2px', marginTop: '5px', opacity: '0.7'}}>
-              <img style={{width: '17px', position: 'sticky'}} src={MobileIcon}/>
-              <img style={{width: '20px', position: 'sticky'}} src={DesktopIcon}/>
+            <div style={{height: '100px', width: '160px', display: 'flex', flexDirection: 'row', margin: '5px', padding: '10px', zIndex: '1', position: 'absolute', justifyContent: 'flex-end', alignItems: 'flex-end', borderRadius: '15px'}}>
+              <img style={{width: '16px', marginRight: '2px', position: 'sticky'}} alt='someicon' src={MobileIcon}/>
+              <img style={{width: '18px', position: 'sticky'}} alt='someicon' src={DesktopIcon}/>
             </div>
           </div>
 
           <div style={{height: '100px', width: '160px', display: 'flex', flexDirection: 'column', margin: '5px', padding: '10px', justifyContent: 'center', alignItems: 'center', backgroundImage: `url(${Puzzle})`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', borderRadius: '15px', border: '1px solid grey'}}>
-            <Link to="/puzzle" target='_blank' className='start-btn' style={{textDecoration: 'none'}}>
+            <Link to="/puzzle" target='_blank' className='start-btn' style={{textDecoration: 'none', zIndex: '3'}}>
               PUZZLE GAME
             </Link>
-            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', gap: '2px', marginTop: '5px', opacity: '0.7'}}>
-              <img style={{width: '17px', position: 'sticky'}} src={MobileIcon}/>
-              <img style={{width: '20px', position: 'sticky'}} src={DesktopIcon}/>
+            <div style={{height: '100px', width: '160px', display: 'flex', flexDirection: 'row', margin: '5px', padding: '10px', zIndex: '1', position: 'absolute', justifyContent: 'flex-end', alignItems: 'flex-end', borderRadius: '15px'}}>
+              <img style={{width: '16px', marginRight: '2px', position: 'sticky'}} alt='someicon' src={MobileIcon}/>
+              <img style={{width: '18px', position: 'sticky'}} alt='someicon' src={DesktopIcon}/>
             </div>
           </div>
 
           <div style={{height: '100px', width: '160px', display: 'flex', flexDirection: 'column', margin: '5px', padding: '10px', justifyContent: 'center', alignItems: 'center', backgroundImage: `url(${Trivia})`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', borderRadius: '15px', border: '1px solid grey'}}>
-            <Link to="/TriviaQuiz" target='_blank' className='start-btn' style={{textDecoration: 'none'}}>
+            <Link to="/TriviaQuiz" target='_blank' className='start-btn' style={{textDecoration: 'none', zIndex: '3'}}>
               TRIVIA APP
             </Link>
-            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', gap: '2px', marginTop: '5px', opacity: '0.7'}}>
-              <img style={{width: '17px', position: 'sticky'}} src={MobileIcon}/>
-              <img style={{width: '20px', position: 'sticky'}} src={DesktopIcon}/>
+            <div style={{height: '100px', width: '160px', display: 'flex', flexDirection: 'row', margin: '5px', padding: '10px', zIndex: '1', position: 'absolute', justifyContent: 'flex-end', alignItems: 'flex-end', borderRadius: '15px'}}>
+              <img style={{width: '16px', marginRight: '2px', position: 'sticky'}} alt='someicon' src={MobileIcon}/>
+              <img style={{width: '18px', position: 'sticky'}} alt='someicon' src={DesktopIcon}/>
             </div>
           </div>
 
           <div style={{height: '100px', width: '160px', display: 'flex', flexDirection: 'column', margin: '5px', padding: '10px', justifyContent: 'center', alignItems: 'center', backgroundImage: `url(${Snake})`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', borderRadius: '15px', border: '1px solid grey'}}>
-            <Link to="/tictactoe" target='_blank' className='start-btn' style={{textDecoration: 'none'}}>
+            <Link to="/tictactoe" target='_blank' className='start-btn' style={{textDecoration: 'none', zIndex: '3'}}>
               SNAKE GAME
             </Link>
-            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', gap: '2px', marginTop: '5px', opacity: '0.7'}}>
-              <img style={{width: '17px', position: 'sticky'}} src={MobileIcon}/>
-              <img style={{width: '20px', position: 'sticky'}} src={DesktopIcon}/>
+            <div style={{height: '100px', width: '160px', display: 'flex', flexDirection: 'row', margin: '5px', padding: '10px', zIndex: '1', position: 'absolute', justifyContent: 'flex-end', alignItems: 'flex-end', borderRadius: '15px'}}>
+              <img style={{width: '16px', marginRight: '2px', position: 'sticky'}} alt='someicon' src={MobileIcon}/>
+              <img style={{width: '18px', position: 'sticky'}} alt='someicon' src={DesktopIcon}/>
             </div>
           </div>
 
           <div style={{height: '100px', width: '160px', display: 'flex', flexDirection: 'column', margin: '5px', padding: '10px', justifyContent: 'center', alignItems: 'center', backgroundImage: `url(${Flappy})`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', borderRadius: '15px', border: '1px solid grey'}}>
-            <Link to="/flappy" target='_blank' className='start-btn' style={{textDecoration: 'none'}}>
+            <Link to="/flappy" target='_blank' className='start-btn' style={{textDecoration: 'none', zIndex: '3'}}>
               FLAPPY BIRD
             </Link>
-            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', gap: '2px', marginTop: '5px', opacity: '0.7'}}>
-              <img style={{width: '17px', position: 'sticky'}} src={MobileIcon}/>
-              <img style={{width: '20px', position: 'sticky'}} src={DesktopIcon}/>
+            <div style={{height: '100px', width: '160px', display: 'flex', flexDirection: 'row', margin: '5px', padding: '10px', zIndex: '1', position: 'absolute', justifyContent: 'flex-end', alignItems: 'flex-end', borderRadius: '15px'}}>
+              <img style={{width: '16px', marginRight: '2px', position: 'sticky'}} alt='someicon' src={MobileIcon}/>
+              <img style={{width: '18px', position: 'sticky'}} alt='someicon' src={DesktopIcon}/>
             </div>
           </div>
         </div>
