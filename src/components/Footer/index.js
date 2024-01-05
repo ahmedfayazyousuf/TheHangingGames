def646e69d1354ca6818e8e270113c35105627a1
ahmedfayazyousuf <95180025+ahmedfayazyousuf@users.noreply.github.com>
2024-01-05 16:59:11 +0400

    p
    p

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div style={{display: 'flex', justifyContent: 'start', alignItems: 'center', bottom: '20px', position: 'absolute', width: '100vw', paddingLeft: '25px', paddingRight: '20px'}}>
         <Link to="/">
-            <button style={{height: '30px', width: '70px', fontSize: '16px', background: 'black', color: '#00f526', border: '1px solid #00f526', cursor: 'grab', marginRight: '15px'}}>Return</button>
+            <button style={{height: '30px', width: '70px', fontSize: '12px', background: 'black', color: '#00f526', border: '1px solid #00f526', cursor: 'grab', marginRight: '15px'}}>Return</button>
         </Link>
         <p style={{color: 'white'}}>The <span style={{color:'#00f526'}}> Hanging </span>House</p>
     </div>
